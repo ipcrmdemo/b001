3d90ae8d96d06afc3e01670e606203ef66d9bd48
Matt Cadorette <mcadorette@gmail.com>
2018-10-11 14:49:58 -0400

    Initial commit from Atomist

---
+++ src/main/java/b001/B001Application.java
@@ -0,0 +1,12 @@
+package b001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B001Application.class, args);
+	}
+}
---
+++ src/test/java/b001/B001ApplicationTests.java
@@ -0,0 +1,16 @@
+package b001;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class B001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
